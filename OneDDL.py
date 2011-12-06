0bf5af6c954ded255fdcd7e5ecc821684596a05f
Nadav <skinnyq@gmail.com>
2011-12-06 19:41:39 +0200

    Updated OneDDL - Parses shows from .ini now

--- OneDDL.py
+++ OneDDL.py
@@ -8,6 +8,56 @@ import httplib2
 import ConfigParser
 import _winreg
 
+def initialize(config):
+    config.read('OneDDL.ini')
+    if not config.has_section('General'):
+        config.add_section('General')
+
+    #Find IDM and add to config
+    try:
+        value = config.get('General', 'IDMPath')
+    except ConfigParser.NoOptionError:
+        value = ''
+    if value.strip() == '' or not os.path.isfile(value):
+        print 'lol'
+        pathi = findIDM()
+        if pathi is None:
+            print 'Couldn\' find IDM'
+            exit(-1)
+        config.set('General', 'IDMPath', pathi)
+
+    #Get default download path, root folder for downloads
+    try:
+        value = config.get('General', 'DefDownloadPath')
+    except ConfigParser.NoOptionError:
+        value = ''
+    if value.strip() == '':
+        print 'Please enter the default download path (eg: D:/Lol/Crap/)'
+        pathd = raw_input('--> ')
+        config.set('General', 'DefDownloadPath', pathd)
+
+    return config
+
+def parseShows(xConfig):
+    showDic = {}
+    shows = [x for x in xConfig.sections() if x != 'General']
+    for show in shows:
+        try:
+            season = xConfig.getint(show, 'Season')
+            episode = xConfig.getint(show, 'Episode')
+            quality = xConfig.get(show, 'Quality')
+        except ConfigParser.NoOptionError:
+            print 'Invalid Show: ', shows
+            continue
+
+        try:
+            pathtd = xConfig.get(show, 'PathToDownload')
+        except ConfigParser.NoOptionError:
+            pathtd = xConfig.get('General', 'DefDownloadPath')
+        showDic[show] = dict('Season' : season, 'Episode': episode, 'Quality' : quality, 'Path' : pathtd)
+
+    return showDic
+                    
 def shouldDownload(title):
     #NotImplementedYet
     return true
@@ -22,42 +72,14 @@ def findIDM():
         return None
 
 config = ConfigParser.SafeConfigParser()
-config.read('OneDDL.ini')
-if not config.has_section('General'):
-    config.add_section('General')
 
-#Find IDM and add to config
-try:
-    value = config.get('General', 'IDMPath')
-except ConfigParser.NoOptionError:
-    value = ''
-if value.strip()s == '' or not os.path.isfile(value):
-    print 'lol'
-    pathi = findIDM()
-    if pathi is None:
-        print 'Couldn\' find IDM'
-        exit(-1)
-    config.set('General', 'IDMPath', pathi)
-
-#Get default download path, root folder for downloads
-try:
-    value = config.get('General', 'DefDownloadPath')
-except ConfigParser.NoOptionError:
-    value = ''
-if value.strip() == '':
-    print 'hai'
-    print 'Please enter the default download path (eg: D:/Lol/Crap/)'
-    pathd = raw_input('--> ')
-    config.set('General', 'DefDownloadPath', pathd) 
+#Set ini file to its initial state
+config = initialize(config)
 
 #Parse TV Shows
+showDic = parseShows(config)
+
 #NotJustYet
-    
-with open('OneDDL.ini', 'wb') as fp:
-    config.write(fp)
-
-#exit(0)
-
 h = httplib2.Http(".cache")
 resp, content = h.request("http://www.oneddl.com/feed/rss/", "GET")
 newcontent = parseString(content)
@@ -81,6 +103,8 @@ for release in newcontent.getElementsByTagName("item"):
                 dict_r[title] = links
 
 print '%s downloads found.' % len(dict_r)
+with open('OneDDL.ini', 'wb') as fp:
+    config.write(fp)
+    
 multipattern = "<div id=\"downloadbutton_\" style=\"\"><a href=\"(.*?)\" onclick=\"launchpopunder\(\)\;\">"
 LinkAdder(pathi, multipattern).start(dict_r.itervalues())
-
